a95d9e2a01bf7071078be6772634e1a38f3d4a0a
JHOKUN <JNHKM10@gmail.com>
2026-07-16 16:37:44 +0200

    Avancée dans l'apprentissage de Flask
    Tout a été lu mais n'a pas pu être testé intégralement

--- FlaskProject/.venv/hello.py
+++ FlaskProject/.venv/hello.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import make_response
+from flask import abort, redirect, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
-
+                                        #  http://127.0.0.1:5000/
 @app.route('/')
 def index():
     return 'Welcome to our home page.\n' \
@@ -11,9 +15,11 @@ def index():
             '/user/<username>\n' \
             '/int_display/<int:int>'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -22,3 +28,25 @@ def show_user_profile(username):
 @app.route('/int_display/<int:int>')
 def show_post(int):
     return f'Parameter : {int}'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/redirect')
+def redirect():
+    return redirect(url_for('login'))
+
+@app.errorhandler(404)
+def not_found(error):
+    resp = make_response(render_template('error.html'), 404)
+    resp.headers['X-Something'] = 'A value'
+    return resp
+
+@app.route('/login')
+def login():
+    abort(401)
